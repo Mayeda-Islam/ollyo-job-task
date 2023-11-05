d7730d68dd268738ee82d96018f119216bdf85f5
unknown <mayedakonika@gmail.com>
2023-11-05 21:31:45 +0600

    photo selected and checked implement

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -16,50 +16,75 @@ const Home = () => {
     {
       id: 1,
       image: img1,
+      status: false,
     },
     {
       id: 2,
       image: img2,
+      status: false,
     },
     {
       id: 3,
       image: img3,
+      status: false,
     },
     {
       id: 4,
       image: img4,
+      status: false,
     },
     {
       id: 5,
       image: img5,
+      status: false,
     },
     {
       id: 6,
       image: img6,
+      status: false,
     },
     {
       id: 7,
       image: img7,
+      status: false,
     },
     {
       id: 8,
       image: img8,
+      status: false,
     },
     {
       id: 9,
       image: img9,
+      status: false,
     },
     {
       id: 10,
       image: img10,
+      status: false,
     },
     {
       id: 11,
       image: img11,
+      status: false,
     },
   ];
   const [gallaryState, setGallaryState] = useState(gallaryArray);
-  console.log(gallaryArray);
+  // const [selectedProduct,setSelectedProduct]=useState()
+
+  const handleCheckboxChange = (id) => {
+    const checkedProduct = gallaryState.map((product) => {
+      if (product.id !== id) {
+        return product;
+      } else {
+        return { ...product, status: !product.status };
+      }
+    });
+
+    setGallaryState(checkedProduct);
+  };
+  console.log(gallaryState);
+
   return (
     <div className="flex justify-center items-end">
       <div className="w-4/5 rounded-lg bg-white ">
@@ -88,12 +113,28 @@ const Home = () => {
               <img
                 src={gallary?.image}
                 alt=""
-                className="w-full h-auto transition duration-300 transform group-hover:scale-105 "
+                className={`${
+                  gallary.status === true
+                    ? "w-full h-auto transition duration-300 transform scale-105 border-none"
+                    : "w-full h-auto transition duration-300 transform group-hover:scale-105 border-none"
+                }`}
               />
-              <div className="absolute inset-0 bg-black  rounded-lg  opacity-0 flex justify-center items-center group-hover:opacity-50">
+              <div
+                className={`${
+                  gallary.status === true
+                    ? "absolute inset-0 bg-black border-none rounded-lg  opacity-20 flex justify-center items-center group-hover:opacity-20"
+                    : "absolute inset-0 bg-black border-none  rounded-lg  opacity-0 flex justify-center items-center group-hover:opacity-20"
+                }`}
+              >
                 <input
                   type="checkbox"
-                  className="transform absolute  scale-150 opacity-100 top-4 left-4 cursor-pointer"
+                  checked={gallary?.status}
+                  onChange={() => handleCheckboxChange(gallary.id)}
+                  className={`${
+                    gallary.status === true
+                      ? "transform absolute scale-150 opacity-100 top-4 left-4 cursor-pointer"
+                      : "transform absolute scale-150 opacity-100 top-4 left-4 cursor-pointer"
+                  }`}
                 />
               </div>
             </div>
